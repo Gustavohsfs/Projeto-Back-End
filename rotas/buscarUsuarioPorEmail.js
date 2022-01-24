716b39ad3b47c66be956b70fa57bae40352e6dd1
guto28 <96759172+guto28@users.noreply.github.com>
2022-01-24 18:00:16 -0300

    Atualizando Codigo

--- rotas/buscarUsuarioPorEmail.js
+++ rotas/buscarUsuarioPorEmail.js
@@ -1,11 +1,11 @@
 const verifyJWT = require('../middleware/verifyJWT')
-const Cadastro = require('../models/function')
+const Exibir = require('../models/function')
  
 module.exports = app => {
     app.post('/usuario/email', verifyJWT, (req, res) => {
         const email = req.body.email
 
-        Cadastro.buscarPorEmail(email)
+        Exibir.buscarPorEmail(email)
             .then(resultados => res.status(200).json(resultados))
             .catch(erro => res.status(400).json(erro))
     })
